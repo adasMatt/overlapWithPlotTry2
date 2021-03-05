2ee422885a69ba7e171883ae28b7498a61a4262a
Matt <madas@hawk.iit.edu>
2021-03-05 12:13:35 -0600

    Initial Commit

---
+++ Shared/overlapWithPlotTry2App.swift
@@ -0,0 +1,17 @@
+//
+//  overlapWithPlotTry2App.swift
+//  Shared
+//
+//  Created by Matthew Adas on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct overlapWithPlotTry2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
